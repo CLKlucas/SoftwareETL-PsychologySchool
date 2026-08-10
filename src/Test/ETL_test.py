80d5ca10e190df3c90f9a34ba4efdb3654e70920
Dev | Lucas Gomes <168451940+CLKlucas@users.noreply.github.com>
2026-08-10 14:55:37 -0300

    introducing Main and new ETL test

---
+++ src/Test/ETL_test.py
@@ -0,0 +1,17 @@
+from src.Conexão import sheet_con
+from src.ETL import extracao
+from src.ETL import transformacao
+
+
+conexao = sheet_con.connect_sheet()
+
+dados_daily = extracao.extrair_daily(conexao)
+
+df = transformacao.transformar_daily(dados_daily)
+
+print("Extração + transformação executadas com sucesso!")
+print(f"Quantidade de registros: {len(df)}")
+print("\nColunas:")
+print(df.columns.tolist())
+print("\nPrimeiras 10 linhas:")
+print(df.head(10).to_string(index=False))
---
+++ src/main.py
@@ -0,0 +1,11 @@
+from Conexão import sheet_con
+from ETL import extracao
+from ETL import transformacao
+
+def main():
+    main()
+
+
+if __name__ == "__main__":
+    print("Iniciando o processo de ETL...")
+   
