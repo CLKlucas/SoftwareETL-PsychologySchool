52bfdd1b623810cd18a3a637c5c30876953dbb36
Dev | Lucas Gomes <168451940+CLKlucas@users.noreply.github.com>
2026-08-10 15:15:34 -0300

    correção

--- src/ETL/extracao.py
+++ src/ETL/extracao.py
@@ -1,13 +1,12 @@
 from src.Conexão import sheet_con
 
-conexão = sheet_con.connect_sheet()
 
-def extrair_daily():
+def extrair_daily(conexão):
     daily = conexão.worksheet("daily")
     dados_daily = daily.get_all_values()
     return dados_daily
 
-def extrair_records():
+def extrair_records(conexão):
     records = conexão.worksheet("records")
     records_dados = records.get_all_values()
     return records_dados
