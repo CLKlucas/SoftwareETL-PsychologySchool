abfba33f7b5f8c6157e58265431ad8514f1adc57
Dev | Lucas Gomes <168451940+CLKlucas@users.noreply.github.com>
2026-08-13 11:41:38 -0300

    test done

--- src/Test/test_load.py
+++ src/Test/test_load.py
@@ -1,7 +1,7 @@
 from src.Conexão import sheet_con
 from src.ETL import extracao
 from src.ETL import transformacao
-from src.ETL import load
+from src.ETL import carregar
 
 
 conexao = sheet_con.connect_sheet()
@@ -15,7 +15,7 @@ df_teste = df.head(2)
 print("Dados que serão enviados:")
 print(df_teste)
 
-resposta = load.carregar_atendimentos(df_teste)
+resposta = carregar.carregar_dados(df_teste)
 
 print("Carga de teste realizada com sucesso.")
 print(resposta.data)
