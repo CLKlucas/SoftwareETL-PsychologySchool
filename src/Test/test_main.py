b17cd5c40cd3affa2cfe0b885f94a11e7e002d45
Dev | Lucas Gomes <168451940+CLKlucas@users.noreply.github.com>
2026-08-13 19:35:15 -0300

    correção de bug

--- src/Test/test_main.py
+++ src/Test/test_main.py
@@ -3,7 +3,7 @@ import main
 
 print("Iniciando teste do ETL completo...")
 
-resultado = main.main()
+resultado = main()
 
 print("ETL completo executado com sucesso.")
 print("Resultado:", resultado)
