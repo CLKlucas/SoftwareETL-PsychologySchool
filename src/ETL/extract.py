0ad485876120d8e27ccc9b0be4304248d66fb778
Dev | Lucas Gomes <168451940+CLKlucas@users.noreply.github.com>
2026-08-24 15:35:50 -0300

    correção da daily

--- src/ETL/extract.py
+++ src/ETL/extract.py
@@ -1,7 +1,7 @@
 from src.Conexão import sheet_con
 
 def extrair_daily(conexão):
-    daily = conexão.worksheet("daily")
+    daily = conexão.worksheet("tabs daily")
     dados_daily = daily.get_all_values()
     return dados_daily
 
